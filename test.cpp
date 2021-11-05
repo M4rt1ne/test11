354611c81f5f001903ec78b5c652920e20fbc4ae
M4rt1ne <romanovskiy2004@gmail.com>
2021-11-05 13:54:58 +0200

    2

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 int main()
 {
-	int x, i, k = 4;
+	int x, i, k = 5;
 	double y;
 	cout << "Input x\n";
 	cin >> x;	
@@ -14,7 +14,7 @@ int main()
 	for (int i = 1; i < k; i++)
 	{
 		y = i + sin(i*x);
-		y = pow(x, 3) + y;
+		y = pow(x, 10) + y;
 		cout << y << endl;
 	}
 }
